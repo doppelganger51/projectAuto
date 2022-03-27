d956bcf378773fa1eb4f60ea944aa4f496ff93b9
karolina3819 <karolina3819@gmail.com>
2022-03-27 13:40:12 +0200

    gotowe Auto

--- Auto.java
+++ Auto.java
@@ -4,7 +4,11 @@ import java.util.Scanner;
 		public String kolor_auta;
 		public int predkosc_max;
 		public int rok_produkcji;
-	
+	public Auto(int a,int b,String c) {
+		this.predkosc_max = a;
+		this.rok_produkcji = b;
+		this.kolor_auta = c;
+	}
 	public Auto() {
         Scanner input = new Scanner(System.in);
 			System.out.println("Podaj predkosc maksymalna:");
@@ -18,27 +22,34 @@ import java.util.Scanner;
         return kolor_auta;
     }
 
-    public void setKolor_auta(String kolor_auta) {
-        this.kolor_auta = kolor_auta;
+    public void setKolor_auta() {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Podaj kolor auta:");
+        this.kolor_auta = input.nextInt();
     }
 
     public int getPredkosc_max() {
         return predkosc_max;
     }
 
-    public void setPredkosc_max(int predkosc_max) {
-        this.predkosc_max = predkosc_max;
+    public void setPredkosc_max() {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Podaj predkosc maksymalna:");
+        this.predkosc_max = input.nextInt();
     }
 	public int getrok_produkcji() {
         return predkosc_max;
     }
 
-    public void setrok_produkcji(int rok_produkcji) {
-        this.rok_produkcji = rok_produkcji;
+    public void setrok_produkcji() {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Podaj rok produkcji:");
+        this.rok_produkcji = input.nextInt();
     }
 
     public String toString(){
         String tekst=" rok produkcji to:"+rok_produkcji+" predkosc maksymalna to:"+predkosc_max+" a kolor auta to :"+kolor_auta;
         return tekst;
     }
-}
+	}
+	}
