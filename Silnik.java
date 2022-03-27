ca94a1dcc2af921ff9b2e76cd58f3463cb86986b
Jaakubb <jakub.tabaka@onet.pl>
2022-03-27 12:56:18 +0200

    3

--- Silnik.java
+++ Silnik.java
@@ -1,11 +1,11 @@
-class.Silnik
 
-import java.util.Scanner
+
+import java.util.Scanner;
 
 public class Silnik{
 	private String rodzaj_silnika;
-	private int numer_serii
-	private int liczba_koni
+	private int numer_serii;
+	private int liczba_koni;
 	
 	public Silnik(){
 		Scanner input = new Scanner(System.in);
@@ -22,19 +22,19 @@ public class Silnik{
 	}
 	
 	public void setrodzaj_silnika(String rodzaj_silnika){
-		this.rodzaj_silnika = rodzaj_silnika
+		this.rodzaj_silnika = rodzaj_silnika;
 	}
 	public int getnumer_serii(){
-		returm numer_serii;
+		return numer_serii;
 	}
 	public void setnumer_serii(int numer_serii){
-		this.numer_serii = numer_serii
+		this.numer_serii = numer_serii;
 	}
 	public int getliczba_koni(){
-		return liczba_koni
+		return liczba_koni;
 	}
 	public void setliczba_koni(int liczba_koni){
-		this.liczba_koni = liczba_koni
+		this.liczba_koni = liczba_koni;
 	}
 	public String toString(){
 		String tekst = "Rodzaj silnika: " + rodzaj_silnika + " Numer serii: " + numer_serii + " Liczba koni: " + liczba_koni;
