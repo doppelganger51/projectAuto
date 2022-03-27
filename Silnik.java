92c0e5c67d285dfde0b563fbd64182a9a9e274d3
Jaakubb <jakub.tabaka@onet.pl>
2022-03-27 12:30:24 +0200

    1

--- Silnik.java
+++ Silnik.java
@@ -1,1 +1,23 @@
-class Silnik
+class.Silnik
+
+import java.util.Scanner
+
+public class Silnik{
+	private String rodzaj_silnika;
+	private int numer_serii
+	private int liczba_koni
+	
+	public Silnik(){
+		Scanner input = new Scanner(System.in);
+		System.out.println("Podaj rodzaj silnika: ");
+		this.rodzaj_silnika = input.next();
+		System.out.println("Podaj numer serii: ");
+		this.numer_serii = Integer.parseInt(input.next());
+		System.out.println("Podaj ilość koni: ");
+		this.liczba_koni = Integer.parse
+		
+	
+	
+	
+	
+
