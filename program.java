99e60d3b9a64aec1c08d3218bd2a1d3ecd15a9d4
doppelganger51 <doppelganger51@gmail.com>
2022-03-27 12:23:26 +0200

    changes_program

--- program.java
+++ program.java
@@ -1,1 +1,20 @@
-class Program
+package program;
+import java.util.Scanner;
+
+class Program{
+public static void main(String[] args) {
+	System.out.println("Co chcesz wyświetlić? (i) - dane auta; (s) - dane silnika; (x) - wyjść z programu");
+	System.out.print("Please input choise: ");
+	String choice = sc.next();
+	if (choice.equals("x")) {
+            System.out.println("Bye");
+	} else {
+		if (choice.equals("i")) {
+//		System.out.println(new newAuto().);
+	}
+	else {
+		if (choice.equals("s")) {
+//		System.out.println(new Silnik().);
+	}
+}
+}
